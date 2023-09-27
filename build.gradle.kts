9ad67812d5d4b8a3269400eab90fe2943018b6f2
virusbear <fabianfahrenhlz718@gmail.com>
2023-09-27 19:42:44 +0200

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    jacoco
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
+    id("org.jetbrains.dokka") version "1.9.0"
+}
+
+group = "com.virusbear"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+    api("io.ktor:ktor-network:2.2.4")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(false)
+    }
+}
+
+detekt {
+    config.setFrom(rootDir.resolve("detekt.yml"))
+    parallel = true
+}
